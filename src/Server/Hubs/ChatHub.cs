680f688bababb743b36a69920c15e90e20bed5f2
kevorkkeheian <kevork.keheian@codgram.com>
2020-11-10 18:06:37 +0200

    ✨ [Feature] add typing message notification - Now users can see that someone is typing

--- src/Server/Hubs/ChatHub.cs
+++ src/Server/Hubs/ChatHub.cs
@@ -8,9 +8,15 @@ namespace Datalk.Server.Hubs
 {
     public class ChatHub : Hub
     {
+        //Send message
         public async Task SendMessage(string chatroomId, string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", chatroomId, user, message);
+        }
+
+        // Notify people in the chatroom that someone is writing
+        public async Task SendTypingMessage(string chatroomId, string user, string message) {
+            await Clients.Others.SendAsync("ReceiveTyping", chatroomId, user, message);
         }
     }
 }
